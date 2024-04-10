58a4280b57a0e255c95e97cbfb376a4d180af31a
Developer-Hannah <157655455+Developer-Hannah@users.noreply.github.com>
2024-04-10 11:17:26 -0600

    filtering

---
+++ Components/ProductCategorysViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LegoMastersPlus.Models;
+using LegoMastersPlus.Data;
+
+namespace LegoMastersPlus.Components
+{
+    public class ProductCategorysViewComponent
+    {
+        public string Invoke()
+        {
+            return "This worked";
+        }
+    }
+}
