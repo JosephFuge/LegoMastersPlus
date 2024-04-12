2d7554d37f2f9a059c1911a117ed856a37d8746c
Joseph Fuge <josephfuge1@gmail.com>
2024-04-11 20:38:49 -0600

    Temp stuff to add role editing

---
+++ Models/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LegoMastersPlus.Models.ViewModels
+{
+    public class EditUserViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Gender is required.")]
+        public string gender { get; set; }
+        // Age will be assumed by birthdate
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Birthdate is required.")]
+        public DateTime birth_date { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Country of Residence is required.")]
+        public string country_of_residence { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First Name is required.")]
+        public string first_name { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name is required.")]
+        public string last_name { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public bool SignInAfter { get; set; } = true;
+    }
+}
